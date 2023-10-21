81f9b0e849712d968b0f4f064e5ca86a6ac450cc
NyongSS <1ss1222@naver.com>
2023-10-21 19:46:02 +0900

    add marker detection xml

---
+++ app/src/main/java/com/example/gukcaptu/markerDetection_2.java
@@ -0,0 +1,14 @@
+package com.example.gukcaptu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class markerDetection_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marker_detection2);
+    }
+}
---
+++ app/src/main/java/com/example/gukcaptu/markerDetection_3.java
@@ -0,0 +1,14 @@
+package com.example.gukcaptu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class markerDetection_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marker_detection3);
+    }
+}
