0f2c6cba40e979ab8853c5c32638a11a61c1f6c8
kche1106 <kche011106@gmail.com>
2023-11-07 12:39:56 +0900

    [feat] put a video on the gymnastics video page

--- app/src/main/java/com/example/nodaji/GymnasticsTrainActivity.java
+++ app/src/main/java/com/example/nodaji/GymnasticsTrainActivity.java
@@ -1,4 +1,13 @@
 package com.example.nodaji;
 
-public class GymnasticsTrainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GymnasticsTrainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gymnastics_train);
+    }
 }
--- app/src/main/java/com/example/nodaji/GymnasticsVideoActivity.java
+++ app/src/main/java/com/example/nodaji/GymnasticsVideoActivity.java
@@ -1,5 +1,52 @@
 package com.example.nodaji;
 
-public class GymnasticsVideoActivity {
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GymnasticsVideoActivity extends AppCompatActivity {
+    VideoView video;
+    Button playButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gymnastics_video);
+
+        video = findViewById(R.id.video);
+        //Video Uri
+        Uri videoUri = Uri.parse("https://kr.object.ncloudstorage.com/hand.gymnastics.video/clap.mov");
+
+        //비디오뷰의 재생, 일시정지 등을 할 수 있는 '컨트롤바'를 붙여주는 작업
+        video.setMediaController(new MediaController(this));
+
+        //VideoView가 보여줄 동영상의 경로 주소(Uri) 설정하기
+        video.setVideoURI(videoUri);
+
+        //동영상을 읽어오는데 시간이 걸리므로..
+        //비디오 로딩 준비가 끝났을 때 실행하도록..
+        //리스너 설정
+        video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                //비디오 시작
+                video.start();
+            }
+        });
+
+        video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Intent intent = new Intent(getApplicationContext(), GymnasticsTrainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
