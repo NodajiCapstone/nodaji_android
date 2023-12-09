3b0bf7dcc0e9c4e0c08002ce9794b9b5f90e35a4
kche1106 <kche011106@gmail.com>
2023-12-09 19:07:00 +0900

    [feat] implementing the hand gymnastics results screen

--- app/src/main/java/com/example/nodaji/GymnasticsWrongActivity.java
+++ app/src/main/java/com/example/nodaji/GymnasticsWrongActivity.java
@@ -1,4 +1,33 @@
 package com.example.nodaji;
 
-public class GymnasticsWrongActivity {
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GymnasticsWrongActivity extends AppCompatActivity {
+
+    int video_cnt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gymnastics_wrong);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Intent intent = getIntent();
+        video_cnt = intent.getExtras().getInt("video_cnt");
+
+        Button next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GymnasticsVideoActivity.class);
+                intent.putExtra("video_cnt", video_cnt);
+                startActivity(intent);
+            }
+        });
+    }
 }
