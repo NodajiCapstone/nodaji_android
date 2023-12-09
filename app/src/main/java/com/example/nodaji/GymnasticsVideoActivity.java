13a0146fd57d94f36ee8d1d418456a1c099be0e6
kche1106 <kche011106@gmail.com>
2023-12-09 19:06:11 +0900

    [feat] complete the implementation of hand gymnastics video features

--- app/src/main/java/com/example/nodaji/GymnasticsVideoActivity.java
+++ app/src/main/java/com/example/nodaji/GymnasticsVideoActivity.java
@@ -1,6 +1,7 @@
 package com.example.nodaji;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,15 +15,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class GymnasticsVideoActivity extends AppCompatActivity {
     VideoView video;
-    Button playButton;
+    Button title;
+    String[] action = {"shaking_hands", "count_number", "finger_clap", "moving_fingers", "opp_rock_paper",
+            "rock_clap", "rock_paper"};
+    Integer cnt;
+    Uri videoUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gymnastics_video);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Intent intent = getIntent();
+        try {
+            cnt = intent.getExtras().getInt("video_cnt");
+        } catch (Exception e) {
+            cnt = 1;
+        }
+        System.out.println("cnt = " + cnt);
 
         video = findViewById(R.id.video);
+        title = findViewById(R.id.title);
         //Video Uri
-        Uri videoUri = Uri.parse("https://kr.object.ncloudstorage.com/hand.gymnastics.video/clap.mov");
+        if(cnt == 1) {
+            videoUri = Uri.parse("https://kr.object.ncloudstorage.com/hand.gymnastics.video/shaking_hands.mov");
+        } else if (cnt == 2) {
+            title.setText("2. 숫자 세기 운동");
+            videoUri = Uri.parse("https://kr.object.ncloudstorage.com/hand.gymnastics.video/count_number.mov");
+        } else if (cnt == 3) {
+            title.setText("3. 손가락 박수 운동");
+            videoUri = Uri.parse("https://kr.object.ncloudstorage.com/hand.gymnastics.video/finger_clap.mov");
+        } else if (cnt == 4) {
+            title.setText("4. 손가락 움직이기 운동");
+            videoUri = Uri.parse("https://kr.object.ncloudstorage.com/hand.gymnastics.video/moving_fingers.mov");
+        } else if (cnt == 5) {
+            title.setText("5. 주먹 보자기 뒤집기 운동");
+            videoUri = Uri.parse("https://kr.object.ncloudstorage.com/hand.gymnastics.video/opp_rock_paper.mov");
+        } else if (cnt == 6) {
+            title.setText("6. 주먹 박수 운동");
+            videoUri = Uri.parse("https://kr.object.ncloudstorage.com/hand.gymnastics.video/rock_clap.mov");
+        } else if (cnt == 7) {
+            title.setText("7. 주먹 보자기 운동");
+            videoUri = Uri.parse("https://kr.object.ncloudstorage.com/hand.gymnastics.video/rock_paper.mov");
+        }
 
         //비디오뷰의 재생, 일시정지 등을 할 수 있는 '컨트롤바'를 붙여주는 작업
         video.setMediaController(new MediaController(this));
@@ -30,9 +66,6 @@ public class GymnasticsVideoActivity extends AppCompatActivity {
         //VideoView가 보여줄 동영상의 경로 주소(Uri) 설정하기
         video.setVideoURI(videoUri);
 
-        //동영상을 읽어오는데 시간이 걸리므로..
-        //비디오 로딩 준비가 끝났을 때 실행하도록..
-        //리스너 설정
         video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mediaPlayer) {
@@ -45,6 +78,8 @@ public class GymnasticsVideoActivity extends AppCompatActivity {
             @Override
             public void onCompletion(MediaPlayer mp) {
                 Intent intent = new Intent(getApplicationContext(), GymnasticsTrainActivity.class);
+                intent.putExtra("action", action[cnt-1]);
+                intent.putExtra("video_cnt", cnt);
                 startActivity(intent);
             }
         });
