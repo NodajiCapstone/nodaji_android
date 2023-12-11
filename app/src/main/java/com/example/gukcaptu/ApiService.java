c65e3d1e9e57c4424821224199002776ecd52d1b
kche1106 <kche011106@gmail.com>
2023-12-11 22:07:31 +0900

    [merge] merge MarkerDetection into main

--- app/src/main/java/com/example/gukcaptu/ApiService.java
+++
@@ -1,9 +0,0 @@
-package com.example.gukcaptu;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface ApiService {
-    @GET("/api/endpoint")
-    Call<String> getServerData();
-}
--- app/src/main/java/com/example/gukcaptu/ParticipantList.java
+++
@@ -1,4 +0,0 @@
-package com.example.gukcaptu;
-
-public class ParticipantList {
-}
