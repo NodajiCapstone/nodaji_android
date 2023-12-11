00215f6ef852d9d8a801c58ebaa20b85b3898e97
kche1106 <kche011106@gmail.com>
2023-12-11 20:14:59 +0900

    [feat] implement Kakao logout features

--- app/src/main/java/com/example/nodaji/MainActivity.java
+++ app/src/main/java/com/example/nodaji/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.nodaji;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,21 +12,22 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import com.kakao.sdk.user.UserApiClient;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView name = findViewById(R.id.name);
-        AppCompatButton hand_gymnastics = findViewById(R.id.hand_gymnastics);
-        AppCompatButton facial_expression = findViewById(R.id.facial_expression);
-
+        /* 사용자 이름 설정 */
         Intent intent = getIntent();
         String strUserName = intent.getStringExtra("name");
-        System.out.println("main " + strUserName);
+        TextView name = findViewById(R.id.name);
         name.setText(strUserName);
 
+        /* 손 체조 학습하기 */
+        AppCompatButton hand_gymnastics = findViewById(R.id.hand_gymnastics);
         hand_gymnastics.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /* 감정 분석하기 */
+        AppCompatButton facial_expression = findViewById(R.id.facial_expression);
         facial_expression.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,5 +46,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        AppCompatButton logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UserApiClient.getInstance().logout(error -> {
+                    if (error != null) {
+                        Log.e(TAG, "로그아웃 실패, SDK에서 토큰 삭제됨", error);
+                    }else{
+                        Log.e(TAG, "로그아웃 성공, SDK에서 토큰 삭제됨");
+                    }
+                    return null;
+                });
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
